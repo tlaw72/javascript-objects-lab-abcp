cca0aed1a9cb9782a5afce6469879b404b10e3e0
Troy Lawrence <redirect@gmail.com>
2018-02-06 18:55:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+let recipes = {
+  prop: 1
+  
+}
+
+function updateObjectWithKeyAndValue (object, key, value) {
+   object.assign(object, {key: 'value'});
+   return object;
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+   Object.assign(Object, {key: 'value'});
+   return Object;
+}
+
+function deleteFromObjectByKey (object, key) {
+  return delete Object.key;
+}
+
+function destructivelyDeleteFromObjectByKey (object, key) {
+  return delete Object.key;
+}
